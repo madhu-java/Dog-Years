a8945ebd5828dcf5ece883e677e5617aa7cb1760
madhu <52275671+madhu-java@users.noreply.github.com>
2020-05-20 15:20:26 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const myAge = 3; //my human age
+let  earlyYears = 2;//early 2 years
+earlyYears *= 10.5;
+let lastYears= myAge-2;//taking away early2 years from actual human age
+lastYears*=4;
+//console.log(earlyYears);
+//console.log(lastYears);
+const myDogYears = earlyYears + lastYears;//adding early years and lastyears to get total dog years
+myName = 'Arjun'.toLowerCase();//toLowerCase() method os string returend the string in lowercase.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myDogYears} years old in dog years`);
